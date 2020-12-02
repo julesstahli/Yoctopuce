6efb45615ee3eb049c69856d55f0bc1151cb3525
Jules Stahli <47100352+julesstahli@users.noreply.github.com>
2020-12-02 14:08:07 +0100

    Initial commit

---
+++ Yoctopuce/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yoctopuce
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            string errsmg = "";
+            if (YAPI.RegisterHub("usb", ref errsmg) == YAPI.SUCCESS)
+                Application.Run(new Main());
+            else
+                MessageBox.Show("Init error:" + errsmg);
+        }
+    }
+}
