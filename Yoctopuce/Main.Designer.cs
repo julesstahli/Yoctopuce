65a412b32c1558b584a3be1e6391e16a0fd0f3f8
Jeremy Meissner <34174370+JeremyMeissner@users.noreply.github.com>
2020-12-09 12:14:54 +0100

    Intergrate a timer

--- Yoctopuce/Main.Designer.cs
+++ Yoctopuce/Main.Designer.cs
@@ -29,22 +29,45 @@ namespace Yoctopuce
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.lblTemperature = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // lblTemperature
+            // 
+            this.lblTemperature.AutoSize = true;
+            this.lblTemperature.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemperature.Location = new System.Drawing.Point(289, 172);
+            this.lblTemperature.Name = "lblTemperature";
+            this.lblTemperature.Size = new System.Drawing.Size(204, 73);
+            this.lblTemperature.TabIndex = 0;
+            this.lblTemperature.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTemperature);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
             this.Text = "Yoctopuce";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTemperature;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Yoctopuce/Main.cs
+++ Yoctopuce/Main.cs
@@ -13,13 +13,22 @@ namespace Yoctopuce
 {
     public partial class Main : Form
     {
+        Temperature temperatureSensor;
         public Main()
         {
             InitializeComponent();
 
             // Test
-            Temperature temperatureSensor = new Temperature();
+            temperatureSensor = new Temperature();
             Console.WriteLine(temperatureSensor.ToString());
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (temperatureSensor != null)
+                lblTemperature.Text = temperatureSensor.ToString();
+            else
+                lblTemperature.Text = "OFFLINE";
         }
     }
 }
