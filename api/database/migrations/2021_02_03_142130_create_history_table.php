8263d76500fcaea2d536d0732406ded3476afa9d
Samuel.pschr <samuel.pschr@eduge.ch>
2021-02-03 15:26:45 +0100

    Add new table in data base

---
+++ api/database/migrations/2021_02_03_142130_create_history_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('history', function (Blueprint $table) {
+            $table->id();
+            $table->decimal("temperature", 20, 10);
+            $table->decimal("pression", 20, 10);
+            $table->decimal("humidity", 20, 10);
+            $table->decimal("brightness", 20, 10);
+            $table->timestamp('date');
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('history');
+    }
+}
