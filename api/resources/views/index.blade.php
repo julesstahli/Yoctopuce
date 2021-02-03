26a47dc4349bf9b00a300f8041739d36f72c50d0
Jeremy Meissner <34174370+JeremyMeissner@users.noreply.github.com>
2021-02-03 14:01:09 +0100

    Add the hudimity chart

--- api/resources/views/index.blade.php
+++ api/resources/views/index.blade.php
@@ -23,15 +23,60 @@
       </tbody>
     </table>
     <div class="">
-      <canvas id="myChart" width="400" height="400"></canvas>
+      <canvas id="temperatureChart" width="400" height="400"></canvas>
+      <canvas id="humidityChart" width="400" height="400"></canvas>
 
     </div>
   </div>
   <script type="text/javascript">
+/*
+  class YoctoChart {
+    static chartList = [];
+    constructor(parent, name) {
+      let tempChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          lineColor : "#fffff",
+          datasets: [{
+            label: name,
+            backgroundColor: 'rgba(210, 210, 210, 0.25)',
+            borderColor: 'rgba(210, 210, 210)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          maintainAspectRatio: false,
+          scales: {
+            yAxes: [{
+              ticks: {
+                suggestedMin: 20,
+                suggestedMax: 25,
+                stepSize: 1
+              }
+            }]
+          }
+        }
+      });
+      tempChart.canvas.parentNode.style.width = '50%';
+      tempChart.canvas.parentNode.style.height = '500px';
+      chartList.push(tempChart);
+    }
+    static ShowChartList() {
+      this.chartList.forEach((e) => {
+
+      });
+
+    }
+
+  }
+  let c = new YoctoChart(null, "temperature");
+*/
+
+  let chartList = [];
 
 
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart = new Chart(ctx, {
+  let temperatureContext = document.getElementById('temperatureChart').getContext('2d');
+  let temperatureChart = new Chart(temperatureContext, {
     type: 'line',
     data: {
       lineColor : "#fffff",
@@ -56,9 +101,40 @@
     }
   });
 
+  let humidityContext = document.getElementById('humidityChart').getContext('2d');
+  let humidityChart = new Chart(humidityContext, {
+    type: 'line',
+    data: {
+      lineColor : "#fffff",
+      datasets: [{
+        label: 'humidity',
+        backgroundColor: 'rgba(175, 175, 210, 0.25)',
+        borderColor: 'rgba(175, 175, 210)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [{
+          ticks: {
+            suggestedMin: 20,
+            suggestedMax: 25,
+            stepSize: 1
+          }
+        }]
+      }
+    }
+  });
+
   // Implementation de la chart en HTML
-  myChart.canvas.parentNode.style.width = '50%';
-  myChart.canvas.parentNode.style.height = '500px';
+  temperatureChart.canvas.parentNode.style.width = '50%';
+  temperatureChart.canvas.parentNode.style.height = '400px';
+
+  humidityChart.canvas.parentNode.style.width = '50%';
+  humidityChart.canvas.parentNode.style.height = '400px';
+
+  chartList.push(temperatureChart);
 
   // Fonction qui ajoute de la data dans la chart
   function addData(chart, label, data) {
@@ -74,10 +150,14 @@
     response.data.reverse();
     response.data.forEach((data) => {
       let date = new Date(data["created_at"]);
-      addData(myChart, `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`, data["temperature"]);
+      addData(temperatureChart, `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`, data["temperature"]);
+      addData(humidityChart, `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`, data["humidity"]);
     });
-    myChart.options.scales.yAxes[0]["ticks"].suggestedMin = response.data[0]["temperature"]*1;
-    myChart.options.scales.yAxes[0]["ticks"].suggestedMax = response.data[0]["temperature"]*1;
+    temperatureChart.options.scales.yAxes[0]["ticks"].suggestedMin = response.data[0]["temperature"]*1;
+    temperatureChart.options.scales.yAxes[0]["ticks"].suggestedMax = response.data[0]["temperature"]*1;
+
+    humidityChart.options.scales.yAxes[0]["ticks"].suggestedMin = response.data[0]["humidity"]*1;
+    humidityChart.options.scales.yAxes[0]["ticks"].suggestedMax = response.data[0]["humidity"]*1;
   });
 
 
@@ -125,19 +205,27 @@
       let date = new Date(response.data["created_at"]);
 
       // Ajoute le label en bas de la date de la dernière mesure (ajoute a droite dans la chart)
-      myChart.data.labels.push(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+      temperatureChart.data.labels.push(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+      humidityChart.data.labels.push(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
 
       // Supprime le label de la date tout a gauche (ca permet de garder un nombre constant d'informations dans la chart)
-      myChart.data.labels.shift();
+      temperatureChart.data.labels.shift();
+      humidityChart.data.labels.shift();
 
       // Ajoute la valeur de la dernière mesure tout a gauche et enlève celle à droite
-      myChart.data.datasets.forEach((dataset) => {
+      temperatureChart.data.datasets.forEach((dataset) => {
         dataset.data.push(response.data["temperature"]);
         dataset.data.shift();
       });
 
+      humidityChart.data.datasets.forEach((dataset) => {
+        dataset.data.push(response.data["humidity"]);
+        dataset.data.shift();
+      });
+
       // Met à jour l'affichage de la chart
-      myChart.update();
+      temperatureChart.update();
+      humidityChart.update();
     });
 
   }, 1000);
