80f49e359848a5fc9a5e82769cbacc76ab367833
Jeremy Meissner <34174370+JeremyMeissner@users.noreply.github.com>
2021-02-10 12:37:03 +0100

    Finished the api id data verification

--- api/resources/views/index.blade.php
+++ api/resources/views/index.blade.php
@@ -29,49 +29,6 @@
     </div>
   </div>
   <script type="text/javascript">
-/*
-  class YoctoChart {
-    static chartList = [];
-    constructor(parent, name) {
-      let tempChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          lineColor : "#fffff",
-          datasets: [{
-            label: name,
-            backgroundColor: 'rgba(210, 210, 210, 0.25)',
-            borderColor: 'rgba(210, 210, 210)',
-            borderWidth: 1
-          }]
-        },
-        options: {
-          maintainAspectRatio: false,
-          scales: {
-            yAxes: [{
-              ticks: {
-                suggestedMin: 20,
-                suggestedMax: 25,
-                stepSize: 1
-              }
-            }]
-          }
-        }
-      });
-      tempChart.canvas.parentNode.style.width = '50%';
-      tempChart.canvas.parentNode.style.height = '500px';
-      chartList.push(tempChart);
-    }
-    static ShowChartList() {
-      this.chartList.forEach((e) => {
-
-      });
-
-    }
-
-  }
-  let c = new YoctoChart(null, "temperature");
-*/
-
   let chartList = [];
 
 
@@ -162,71 +119,74 @@
 
 
 
-
-
-
+  let measureBefore;
 
   // Chaque seconde, ajouter la dernière mesure de la base de donnée
   setInterval(() => {
-    axios.get("/api/measure").then(response => {
-
-      // Prend la table actuel
-      let tbl = document.getElementById("tbl");
-
-      // Créer un <tr>
-      let tr = document.createElement("tr");
-
-      for(let dataName of ["temperature", "brightness", "humidity", "pression", "created_at"]){
-        // Creer un nouveau cellule dans le <tr> du tableau
-        let td = document.createElement("td");
-
-        // Ajoute la valeur de la requete à l'api dans le <td> créé précédement
-        td.innerHTML = response.data[dataName];
-
-        // Ajoute le <td> dans le <tr>
-        tr.appendChild(td);
-      }
-
-      // Ajoute au début du tableau le <tr> créé juste au dessus
-      tbl.insertBefore(tr, tbl.firstChild);
-
-
-      // Si le tableau a plus de 1 ligne, enlever la dernière (permet de garder le plus récent uniquement à chaque fois)
-      if (tbl.rows.length > 1) {
-
-        tbl.lastElementChild.remove();
-      }
-    });
 
     // Recupere la dernière mesure de la base de donnée
     axios.get("/api/measure").then(response => {
 
-      // Converti la date de la base de donnée en un format utilisable (format JS)
-      let date = new Date(response.data["created_at"]);
+      // Tester si la valeur a été ajouté juste avant
+      if (measureBefore != response.data["id"]) {
 
-      // Ajoute le label en bas de la date de la dernière mesure (ajoute a droite dans la chart)
-      temperatureChart.data.labels.push(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
-      humidityChart.data.labels.push(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
 
-      // Supprime le label de la date tout a gauche (ca permet de garder un nombre constant d'informations dans la chart)
-      temperatureChart.data.labels.shift();
-      humidityChart.data.labels.shift();
 
-      // Ajoute la valeur de la dernière mesure tout a gauche et enlève celle à droite
-      temperatureChart.data.datasets.forEach((dataset) => {
-        dataset.data.push(response.data["temperature"]);
-        dataset.data.shift();
-      });
+        // Prend la table actuel
+        let tbl = document.getElementById("tbl");
 
-      humidityChart.data.datasets.forEach((dataset) => {
-        dataset.data.push(response.data["humidity"]);
-        dataset.data.shift();
-      });
+        // Créer un <tr>
+        let tr = document.createElement("tr");
 
-      // Met à jour l'affichage de la chart
-      temperatureChart.update();
-      humidityChart.update();
+        for(let dataName of ["temperature", "brightness", "humidity", "pression", "created_at"]){
+          // Creer un nouveau cellule dans le <tr> du tableau
+          let td = document.createElement("td");
+
+          // Ajoute la valeur de la requete à l'api dans le <td> créé précédement
+          td.innerHTML = response.data[dataName];
+
+          // Ajoute le <td> dans le <tr>
+          tr.appendChild(td);
+        }
+
+        // Ajoute au début du tableau le <tr> créé juste au dessus
+        tbl.insertBefore(tr, tbl.firstChild);
+
+
+        // Si le tableau a plus de 1 ligne, enlever la dernière (permet de garder le plus récent uniquement à chaque fois)
+        if (tbl.rows.length > 1) {
+          tbl.lastElementChild.remove();
+        }
+
+        // Converti la date de la base de donnée en un format utilisable (format JS)
+        let date = new Date(response.data["created_at"]);
+
+        // Ajoute le label en bas de la date de la dernière mesure (ajoute a droite dans la chart)
+        temperatureChart.data.labels.push(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+        humidityChart.data.labels.push(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+
+        // Supprime le label de la date tout a gauche (ca permet de garder un nombre constant d'informations dans la chart)
+        temperatureChart.data.labels.shift();
+        humidityChart.data.labels.shift();
+
+        // Ajoute la valeur de la dernière mesure tout a gauche et enlève celle à droite
+        temperatureChart.data.datasets.forEach((dataset) => {
+          dataset.data.push(response.data["temperature"]);
+          dataset.data.shift();
+        });
+
+        humidityChart.data.datasets.forEach((dataset) => {
+          dataset.data.push(response.data["humidity"]);
+          dataset.data.shift();
+        });
+
+        // Met à jour l'affichage de la chart
+        temperatureChart.update();
+        humidityChart.update();
+      }
+      measureBefore = response.data["id"];
     });
+
 
   }, 1000);
 
